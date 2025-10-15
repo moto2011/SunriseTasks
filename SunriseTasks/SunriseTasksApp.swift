843dbd732278647ca2860421bd58379e3c3900b9
Minoru Nakamura <nakamura200701@gmail.com>
2025-10-15 23:53:23 +0900

    first commit

---
+++ SunriseTasks/SunriseTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunriseTasksApp.swift
+//  SunriseTasks
+//
+//  Created by nakamura on 2025/10/15.
+//
+
+import SwiftUI
+
+@main
+struct SunriseTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
